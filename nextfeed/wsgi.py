c3e17968619dcca65c134a1aa3937ba8cb5de4c1
Amir Rachum <amir@rachum.com>
2013-08-15 21:59:04 +0300

    wsgi

--- nextfeed/wsgi.py
+++ nextfeed/wsgi.py
@@ -13,16 +13,11 @@ middleware here, or combine a Django application with an application of another
 framework.
 
 """
-# import os
-
-# from django.core.wsgi import get_wsgi_application
-# from dj_static import Cling
-
-# os.environ.setdefault("DJANGO_SETTINGS_MODULE", "nextfeed.settings")
-
-# application = Cling(get_wsgi_application())
+import os
 
 from django.core.wsgi import get_wsgi_application
 from dj_static import Cling
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "nextfeed.settings")
 
 application = Cling(get_wsgi_application())
