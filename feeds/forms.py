017866807eadc499666244ffda00867b87a1d84d
Amir Rachum <amir@rachum.com>
2013-08-24 17:18:45 +0300

    StickyFooter. Rewrote most of the CSS/HTML.

--- feeds/forms.py
+++ feeds/forms.py
@@ -1,8 +1,13 @@
 from django.forms.models import ModelForm
 from feeds.models import Feed
+from django.forms.widgets import TextInput
 
 
 class FeedForm(ModelForm):
     class Meta:
         model = Feed
         fields = ['link']
+        widgets = {
+            'link': TextInput(attrs={'placeholder': 'Paste RSS address here!',
+                                     'id': 'feed-url-input'})
+        }
