76a11701600e55b09b4feef4cd8fd6f3881a4555
Amir Rachum <amir@rachum.com>
2013-08-31 20:40:46 +0300

    Remove selected button - doesn't do anything yet.

--- feeds/forms.py
+++ feeds/forms.py
@@ -1,6 +1,10 @@
 from django.forms.models import ModelForm
-from feeds.models import Feed
 from django.forms.widgets import TextInput
+
+import feedparser
+
+from feeds.models import Feed
+from django.core.exceptions import ValidationError
 
 
 class FeedForm(ModelForm):
@@ -11,3 +15,10 @@ class FeedForm(ModelForm):
             'link': TextInput(attrs={'placeholder': 'Paste RSS address here!',
                                      'id': 'feed-url-input'})
         }
+
+    def clean(self):
+        cleaned_data = super(FeedForm, self).clean()
+        parser = feedparser.parse(cleaned_data.get('link'))
+        if parser.feed == {}:
+            raise ValidationError("The link provided is not a valid feed.")
+        return cleaned_data
