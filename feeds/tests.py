d31cb3703ff40c4935fc3b2b4bf7f5070d27a8dc
Amir Rachum <amir@rachum.com>
2013-09-26 17:11:39 +0300

    Started working on a test

--- feeds/tests.py
+++ feeds/tests.py
@@ -1,16 +1,46 @@
-"""
-This file demonstrates writing tests using the unittest module. These will pass
-when you run "manage.py test".
+"""Feed-related tests."""
 
-Replace this with more appropriate tests for your application.
-"""
-
+import mock
+from collections import namedtuple
 from django.test import TestCase
 
+from feeds import tasks
+from feeds.models import Feed, Entry
+from profiles.models import UserEntryDetail
 
-class SimpleTest(TestCase):
-    def test_basic_addition(self):
-        """
-        Tests that 1 + 1 always equals 2.
-        """
-        self.assertEqual(1 + 1, 2)
+FeedParserEntry = namedtuple('FeedParserEntry','link title published_parsed')
+
+
+class PollFeedTest(TestCase):
+    def setUp(self):
+        self.feed = Feed(link="http://link.to/rss.xml",
+                         title="A Feed")
+        self.feed.save()
+                         
+    
+    @mock.patch('feedparser.parse')
+    def test_successful_poll(self, mock_parser):
+        """Test a successful polling of a feed."""
+        
+        # Set up mock data
+        published = ()
+        feedparser_entry = FeedParserEntry(title="An Entry",
+                                           link="http://entry",
+                                           published_parsed=published)
+        mock_parser.entries = [feedparser_entry]
+        
+        # Verify initial state
+        self.assertEqual(Feed.objects.count(), 1)
+        self.assertEqual(Entry.objects.count(), 0)
+        self.assertEqual(UserEntryDetail.objects.count(), 0)
+        
+        # Perform poll
+        tasks.poll_feed(self.feed)
+        
+        # Verify final state
+        self.assertEqual(Feed.objects.count(), 1)
+        self.assertEqual(Entry.objects.count(), 1)
+        self.assertEqual(UserEntryDetail.objects.count(), 0)
+        
+        
+        
