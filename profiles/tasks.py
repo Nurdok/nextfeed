bfb0afe9b05e8d2f01863ffeab8ed1aee0382ccf
Amir Rachum <amir@rachum.com>
2014-03-01 23:19:01 +0200

    issue reporting - error handling.

--- profiles/tasks.py
+++ profiles/tasks.py
@@ -5,7 +5,6 @@ import github
 
 @task
 def report_issue(summary, details):
-    print 'creating an issue "{}"/"{}"'.format(summary, details)
     gh = github.Github(settings.GITHUB_API_KEY)
     repo = gh.get_repo("Nurdok/nextfeed")
     repo.create_issue(summary, details)
--- static/nextfeed/js/report_issue.js
+++ static/nextfeed/js/report_issue.js
@@ -12,14 +12,19 @@ var ReportIssueModalController = function ($scope, $http, $modalInstance,
                                            $timeout) {
     $scope.button_text = "Send";
     $scope.is_sending = false;
+    $scope.button_type = "primary"
 
     $scope.ok = function (summary, details) {
         $scope.is_sending = true;
         $scope.button_text = "Sending...";
         var data = {summary: summary, details: details}
-        $http({method: 'POST', url: 'report', data: data}).
-            success(function(data, status, headers, config) {
+        $http({method: 'POST', url: 'report', data: data})
+            .success(function(data, status, headers, config) {
                 $scope.button_text = "Sent!";
+            })
+            .error(function(data, status, headers, config) {
+                $scope.button_text = "Error! Try again later."
+                $scope.button_type = "danger"
             });
         $timeout($modalInstance.close, 3000)
     };
